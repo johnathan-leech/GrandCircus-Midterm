583bed0a2be6333d313cdf88766cd11944acada9
Johnathan Leech <johnathanleech09@gmail.com>
2018-10-25 16:25:20 -0400

    added methods to Board Class

--- Midterm/Board.cs
+++ Midterm/Board.cs
@@ -1,7 +1,97 @@
 ﻿namespace Midterm
 {
+    public enum State { hidden, flag, qmark, clicked }
+
     class Board
     {
+        private int rows;
+        private int columns;
+        private double minesPercent;
+        private int[,] hiddenBoard;
+        private State[,] displayBoard;
+
+
+        //private int rows;
+        //public int Rows
+        //{
+        //    get
+        //    {
+        //        return rows;
+        //    }
+        //    set
+        //    {
+        //        rows = value;
+        //    }
+        //}
+
+        //private int columns;
+        //public int Columns
+        //{
+        //    get
+        //    {
+        //        return columns;
+        //    }
+        //    set
+        //    {
+        //        columns = value;
+        //    }
+        //}
+
+        public Board()
+        {
+            rows = 10;
+            columns = 10;
+            minesPercent = .15;
+        }
+
+        public Board(int row, int column) { }
+
+        public Board(int row, int column, int minesPercent) { }
+
+        private void InitializeBoard()
+        {
+            int[,] hiddenBoard = new int[10, 10];
+
+            int[,] displayBoard = new int[10, 10];
+        }
+
+
+        public void DisplayBoard()
+        {
+            // will use switch-case statement to display 'clicked' 'hidden' 'qmark' 'flag'
+        }
+
+        // this method would work to reveal if the tile is a flag, qmark, number, bomb
+        public void RevealTile(int row, int column) { }
+
+        private bool MakesMine() { return false; }
+
+        public bool CompletedGame()
+        {
+            // will tell user if they won or lost
+            return false;
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     }
 }
